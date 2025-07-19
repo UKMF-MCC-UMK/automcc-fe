a225e0a2ace51172e33296e3329aad0e6c6e3c3a
Reazon Gd <cah.gaming707@gmail.com>
2025-07-19 14:36:06 +0700

    refactor: consolidate styles and scripts across HTML files, implement fetch utility for API calls

--- env.js
+++ env.js
@@ -1,2 +1,29 @@
-const BACKEND_URL = 'https://backend.study-mcc.my.id/api';
-const BACKEND_STORAGE_URL = 'https://backend.study-mcc.my.id/storage';
+const BACKEND_URL = "http://localhost:8000/api";
+const BACKEND_STORAGE_URL = "http://localhost:8000/storage";
+
+// upload configuration
+const MAX_FILE_SIZE = 1024 * 1024 * 2; // => limiting each file (bytes); set zero (0) to disable
+const FILE_TYPE_ALLOWED = [
+  // html css js
+  "text/html",
+  "text/css",
+  "text/javascript",
+  "application/javascript",
+  //   image
+  "image/png",
+  "image/jpeg",
+  "image/jpg",
+  "image/gif",
+  "image/bmp",
+  "image/webp",
+  "image/tiff",
+  "image/svg+xml",
+  // Fonts
+  "font/woff",
+  "font/woff2",
+  "font/ttf",
+  "font/otf",
+  // other
+  "text/plain",
+  "application/json",
+];
